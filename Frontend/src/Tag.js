24f0880f309c30939d1f18c43312b185570b77fa
codyswanner <108250848+codyswanner@users.noreply.github.com>
2023-09-19 20:14:53 -0500

    Comment Tag.js Added some comments to Tag.js.

--- Frontend/src/Tag.js
+++ Frontend/src/Tag.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function Tag(props) {
     if (props.tag === 0) {
+        // The zero tag is assigned to pictures with no other tags.
+        // Display a prompt to add a tag.
       return(
         <div style={{ fontSize: "0.7rem", color: "white" }}>
           <p>{'Add tag'}</p>
         </div>
       );
     } else {
+        // Display the name of the tag that was passed in.
       const tagId = props.tag;
-      const tagName = tagId ? props.tagNames.find(tagData => tagData.id === tagId)['name'] : 'Add Tag';
+      const tagName = tagId ? props.tagNames.find(tagData => tagData.id === tagId)['name'] : '!!!'; // If there is no match, show "warning" triple bang
       return (
         <div style={{ fontSize: "0.7rem", color: "white" }}>
           <p>{tagName}</p>
