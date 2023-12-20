8c2e3b73ff159d7a2564fc989f089775ae89ec55
codyswanner <108250848+codyswanner@users.noreply.github.com>
2023-12-19 20:35:24 -0600

    Filtering works, pt 2 Due to an error in the last commit (likely a PEBCAK error) the App.js file was not included in the last commit, which is a central part of making the filters work.  Amending that error now before wrapping this gnarly useEffect into a custom hook and potentially creating a new component to house all this logic, bringing the App component back to a simplistic minimalist state.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -47,7 +47,22 @@ function App(props) {
         // Send filters to Django to make query and return picture ids to display
         // Add user id to this
         socket.send(JSON.stringify({'type': 'activeFilters', 'activeFilters': activeFilters}));
+      } else if (response.type == "applyFilters") {
+        const results = response.results;
+        let imagesToRender = [];
+        if (results.length == 0) {
+          setAppData(props.apiData);
+        } else {props.apiData[0].forEach(image => {
+          if (results.includes(image.id)) {
+            console.log("This one matches: " + image.id)
+            imagesToRender.push(image)
+          };
+        });
+        const newData = [imagesToRender, props.apiData[1], props.apiData[2]];
+        setAppData(newData);
+        }
       }
+      
 
       
     }
