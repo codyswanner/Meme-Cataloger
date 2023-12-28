154eeb3725b66553550e87bacd5302a7980ecf5b
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2023-12-28 15:35:32 -0600

    Extract Components Extracted ImageTopToolbar and ImageBottomToolbar into separate components to clean up ReactivePictureFrame component.

--- Frontend/src/ReactivePictureFrame.js
+++ Frontend/src/ReactivePictureFrame.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Box, IconButton, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
-import Tag from './Tag';
-import AddTagButton from './AddTagButton';
-import ShareIcon from "@mui/icons-material/Share";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Box } from "@mui/material";
+
+import ImageTopToolbar from './ImageTopToolbar';
+import ImageBottomToolbar from './ImageBottomToolbar';
 
 
 function ReactivePictureFrame(props) {
@@ -18,20 +16,6 @@ function ReactivePictureFrame(props) {
   const handleImageMouseLeave = () => {
     setImageHovered(false);
   };
-
-  const handleButtonClick = (buttonType, props) => {
-    console.log(`Clicked ${buttonType} on image ${props.index}`);
-  }
-
-  const generateTags = (tags, tagNames) => {
-    return tags.length === 0 ? (
-      <Tag tag={0} key={0} tagNames={tagNames} />
-    ) : (
-      tags.map((tag) => (
-        <Tag tag={tag} key={tag} tagNames={tagNames} />
-      ))
-    );
-  }
 
   var toolbarVisible = imageHovered;
 
@@ -55,33 +39,15 @@ function ReactivePictureFrame(props) {
         onMouseLeave={handleImageMouseLeave}
       >
         {/* Picture top toolbar, includes description and share/archive/trash buttons */}
-        <Toolbar sx={toolbarStyles}>
-          <Typography sx={{ fontSize: "0.9rem", color: "white" }}>
-            Text Description
-          </Typography>
-          <div style={{ marginLeft: "auto" }}>
-            <Toolbar disableGutters>
-              <IconButton onClick={() => handleButtonClick("Share", props)}>
-                <ShareIcon sx={{ color: "white" }} />
-              </IconButton>
-              <IconButton onClick={() => handleButtonClick("Archive", props)}>
-                <ArchiveIcon sx={{ color: "white" }} />
-              </IconButton>
-              <IconButton onClick={() => handleButtonClick("Delete", props)}>
-                <DeleteIcon sx={{ color: "white" }} />
-              </IconButton>
-            </Toolbar>
-          </div>
-        </Toolbar>
+        <ImageTopToolbar toolbarStyles = {toolbarStyles} {...props}/>
+
         {/* Actual image to be displayed */}
         <div>
           <img src={props.src} style={{ maxWidth: props.maxWidth }}/>
         </div>
+
         {/* Picture lower toolbar, includes Tag(s) and AddTagButton */}
-        <Toolbar sx={[toolbarStyles, { bottom: 6.5 }]}>
-          {generateTags(props.tags, props.tagNames)}
-          <AddTagButton index={props.index} style={{ marginLeft: "auto" }}/>
-        </Toolbar>
+        <ImageBottomToolbar toolbarStyles={toolbarStyles} {...props}/>
       </div>
     </Box>
   );
