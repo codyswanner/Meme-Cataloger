724e20989d9cd21089cb829e18acf50a2e1031f0
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-03-01 14:51:22 -0600

    Added upload button Added upload button to top AppBar.  Does not actually have upload logic yet, just a dummy frontend button for now.

--- Frontend/src/ContentFrame.js
+++ Frontend/src/ContentFrame.js
@@ -1,9 +1,23 @@
 import React, { useContext } from 'react';
-import { Box, AppBar, CssBaseline, Toolbar, Typography} from '@mui/material';
+import { Box, Button, AppBar, CssBaseline, Toolbar, Typography} from '@mui/material';
+import UploadIcon from '@mui/icons-material/Upload'
+import { styled } from '@mui/material/styles';
 
 import MainContentArea from './MainContentArea';
 import LeftDrawer from './LeftDrawer';
 import AppDataContext from './AppDataContext';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
 
 function ContentFrame() {
     return (
@@ -16,6 +30,10 @@ function ContentFrame() {
                     <Typography variant='h5' noWrap component='div'>
                         Meme-opolis
                     </Typography>
+                    <Button component="label" variant="contained" sx={{marginLeft: "auto"}} startIcon={<UploadIcon/>}>
+                        Upload
+                        <VisuallyHiddenInput type='file'/>
+                    </Button>
                 </Toolbar>
             </AppBar>
 
