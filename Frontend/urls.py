4650eae8942ed4cde5e26e4be8c247e86c94d6b1
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-05-02 17:44:34 -0500

    docs(Frontend): Add DocStrings Added DocStrings and comments to the "Frontend" python package.  Removed some unnecessary files.

--- Frontend/urls.py
+++ Frontend/urls.py
@@ -1,7 +1,12 @@
+"""Registers package URLs to the Django app.
+Frontend has only an "index" url,
+for providing data for initial page load.
+"""
+
 from django.urls import path
 from . import views
 
-app_name = 'Frontend'
-urlpatterns = [
+app_name: str = 'Frontend'
+urlpatterns: list = [
     path('', views.index, name="index"),
 ]
