9d09cca3967726512c5d816d80a59fbead28febe
codyswanner <108250848+codyswanner@users.noreply.github.com>
2024-05-15 19:17:34 -0500

    fix(Tag): TagOverflow Bug Rather than tags overflowing past the edge of the image, they now condense into a "+n" indicator (where n is the number of tags not shown directly). All tags can still be seen in the tag management popper.

--- Frontend/src/ImageFrame/ImageBottomToolbar.js
+++ Frontend/src/ImageFrame/ImageBottomToolbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Toolbar } from "@mui/material";
+import { Chip, Toolbar } from "@mui/material";
 
 import Tag from './Tag';
 import AddTagButton from './AddTagButton';
+import ExcessTagsChip from './ExcessTagsChip';
 
 
 /**
@@ -23,18 +24,41 @@ function ImageBottomToolbar(props) {
    * @returns A Tag component for each tag associated with this image.
    */
   const generateTags = (imageTags) => {
-    return imageTags.length === 0 ? (
-      <Tag imageTag={0} key={0} />
-    ) : (
+    return(
       imageTags.map((imageTag) => (
         <Tag imageTag={imageTag} imageId={props.id} key={imageTag} />
       ))
     );
   };
 
+  /**
+   * Formats the tags to be displayed on the toolbar.
+   *
+   * @param {array} imageTags Array of tag IDs associated with this image.
+   * @returns Tags of the image, formatted for proper display.
+   */
+  const formatTags = (imageTags) => {
+
+    if (imageTags.length == 0) {
+      return (<Tag imageTag={0} key={0} />); // display "add tag" placeholder
+
+    } else if (imageTags.length < 3) {
+      return generateTags(imageTags); // display normally
+
+    } else {
+      // display condensed, with "+excess" chip
+      return (
+        <>
+          {generateTags(imageTags).slice(0, 2) /* show only two tags */}
+          <ExcessTagsChip labelText={(imageTags.length - 2).toString()} />
+        </>
+      )
+    };
+  };
+
   return(
     <Toolbar sx={[props.toolbarStyles, { bottom: 6.5 }]}>
-      {generateTags(props.imageTags)} {/* Returns Tag components for image */}
+      {formatTags(props.imageTags)} {/* Returns Tag components for image */}
       <AddTagButton imageId={props.id} imageTags={props.imageTags} style={{ marginLeft: "auto" }}/>
     </Toolbar>
   );
