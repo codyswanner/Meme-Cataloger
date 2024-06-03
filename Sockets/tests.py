609dc982cff860bbf893bf786b2b0e9fb432b125
codyswanner <108250848+codyswanner@users.noreply.github.com>
2024-06-03 04:40:03 -0500

    docs(testing): Introducing Automated Testing At long last, automated testing!  Starting with the backend websocket consumer, stay tuned for more developments.

---
+++ Sockets/tests.py
@@ -0,0 +1,49 @@
+"""Tests for the Channels Websocket Consumer."""
+
+import unittest
+
+from consumers import FilterConsumer
+
+
+class TestSocketConsumer(unittest.TestCase):
+    def test_unittest(self):
+        self.assertTrue(True)
+
+    def test_FilterConsumer_connect(self):
+        ...
+
+    def test_FilterConsumer_disconnect(self):
+        ...
+
+    def test_FilterConsumer_filter_change(self):
+        ...
+
+    def test_FilterConsumer_apply_filters(self):
+        ...
+
+    def test_FilterConsumer_add_tag(self):
+        ...
+
+    def test_FilterConsumer_remove_tag(self):
+        ...
+
+    def test_FilterConsumer_create_tag(self):
+        ...
+
+    def test_FilterConsumer_update_tags(self):
+        ...
+
+    def test_FilterConsumer_delete_image(self):
+        ...
+
+    def test_FilterConsumer_update_description(self):
+        ...
+
+    def test_FilterConsumer_receive(self):
+        ...
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
