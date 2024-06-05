6e725f21237f74cb18b729969dfa2382e47360f2
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-06-05 18:02:07 -0500

    test(Sockets): More setup Still trying to get this off the ground.  A little closer today than yesterday.

--- Sockets/tests.py
+++ Sockets/tests.py
@@ -1,21 +1,53 @@
 """Tests for the Channels Websocket Consumer."""
 
-import unittest
+from django.test import TestCase
 
-from consumers import FilterConsumer
+from Sockets.consumers import FilterConsumer
+from api.models import *
 
 
-class TestSocketConsumer(unittest.TestCase):
+class TestSocketConsumer(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        test_user = AppUser.objects.create(username="test_user")
+        image_1 = Image.objects.create(
+            source="/test-image-1.png",
+            owner=test_user,
+            description="I got some banana bread at work today dude, hell yeah"
+        )
+        image_2 = Image.objects.create(
+            source="/test-image-2.jpg",
+            owner=test_user,
+            description="Aw wuold ya luuk at that, anotha loveli dae"
+        )
+        image_3 = Image.objects.create(
+            source="/test-image-3.gif",
+            owner=test_user,
+            description="SHE WORE A CROWN AND SHE CAME DOWN IN A BUBBLE, DOUG"
+        )
+        for i in range(0, 10):
+            Tag.objects.create(name=f"Tag{i}", owner=test_user)
+        for i in [1, 3, 5, 7]:
+            ImageTag.objects.create(image_id=image_1, tag_id=Tag.objects.get(id=i))
+        for i in range(1, 11, 2):
+            ImageTag.objects.create(image_id=image_2, tag_id=Tag.objects.get(id=i))
+        for i in range(1, 11, 3):
+            ImageTag.objects.create(image_id=image_3, tag_id=Tag.objects.get(id=i))
+
     def test_unittest(self):
         self.assertTrue(True)
+
+    def test_setup(self):
+        image_1 = Image.objects.get(id=1)
+        image_2 = Image.objects.get(id=2)
+        self.assertEqual(image_1, image_1)
+        self.assertNotEqual(image_1, image_2)
 
     def test_FilterConsumer_connect(self):
         ...
 
     def test_FilterConsumer_disconnect(self):
-        ...
-
-    def test_FilterConsumer_filter_change(self):
         ...
 
     def test_FilterConsumer_apply_filters(self):
@@ -41,9 +73,3 @@ class TestSocketConsumer(unittest.TestCase):
 
     def test_FilterConsumer_receive(self):
         ...
-
-
-
-
-if __name__ == "__main__":
-    unittest.main()
