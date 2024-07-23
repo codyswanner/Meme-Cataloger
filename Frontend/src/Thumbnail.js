c73c6c4bb140f350f3a6c21dd5368e549d554ad9
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-07-22 20:40:07 -0500

    feat(thumbnails): Neat Boxes! Re-styled the thumbnails to be nice little squares.  Much improved!

--- Frontend/src/Thumbnail.js
+++ Frontend/src/Thumbnail.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
+import { css } from '@emotion/css';
 
+
+const containerStyles = {
+  height: 200,
+  width: 200,
+  overflow: "hidden"
+}
+
+const thumbnailStyles = {
+  display: "block",
+  width: "100%",
+  height: "100%",
+  objectFit: "cover"
+}
 
 function Thumbnail(props) {
+
   return(
     <a href={"http://" + window.location.host + "/image/" + props.id}>
-      <img src={props.src} style={{ maxWidth: 200 }}/>
+    <div style={containerStyles}>
+      <img src={props.src} style={thumbnailStyles}/>
+    </div>
     </a>
   )
 };
