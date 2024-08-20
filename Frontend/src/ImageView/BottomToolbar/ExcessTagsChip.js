acb168259cd74e90c1d0f80dc85e09061d92dfa8
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-08-20 15:19:24 -0500

    test(ExcessTagsChip):  Added test Added a test module for ExcessTagChip.  (Not a lot to test on this one, honestly.)

--- Frontend/src/ImageView/BottomToolbar/ExcessTagsChip.js
+++ Frontend/src/ImageView/BottomToolbar/ExcessTagsChip.js
@@ -3,35 +3,21 @@ import { ThemeProvider, alpha, createTheme } from '@mui/material/styles';
 import Chip from '@mui/material/Chip'
 
 
-function ExcessTagsLabel(props) {
-    const chipLabelStyle = {
-      padding: 0,
-      color: '#ffffff'
-    };
-  
-    return (
-      <p style={chipLabelStyle}>
-        +{props.labelText}
-      </p>
-    );
-}
-
-
 function ExcessTagsChip(props) {
     // Custom coloring to improve tag readability on top of the images
     const chipTheme = createTheme({
         palette: {
           primary: {
-            main: alpha('#333333', 0.6),
-          },
+            main: alpha('#333333', 0.6)
+          }
         }
     });
   
     return (
     <ThemeProvider theme={chipTheme}>
-        <Chip color='primary' label={<ExcessTagsLabel labelText={props.labelText}/>}/>
+        <Chip color='primary' label={<p>+{props.labelText}</p>}/>
     </ThemeProvider>
     );
-}
+};
 
 export default ExcessTagsChip;
