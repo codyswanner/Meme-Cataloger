bd384677b05bf4b19ebc90b514b248691d449e32
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-08-20 17:19:25 -0500

    test(TagDrawer): Resizable window Figured out how to mock changes in window size.  Test suite now ensures that component behaves properly with both large and small screens.

--- Frontend/src/MainPage/TagDrawer/TagDrawer.test.js
+++ Frontend/src/MainPage/TagDrawer/TagDrawer.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import cssMediaQuery from 'css-mediaquery';
 
 import AppDataContext from '../../SupportingModules/AppDataContext';
 import TagDrawer from './TagDrawer';
 
+
 describe('TagDrawer', () => {
     let appData;
     let appState;
-    let component;
+    
     beforeEach(() => {
+        // appData provides info for items to be rendered
         appData = {
             imageData: [], // not used in this component
             tagData: [
@@ -19,42 +22,153 @@ describe('TagDrawer', () => {
             ],
             imageTagData: [] // not used in this component
         };
-        appState = { drawerOpen: false };
+        appState = {drawerOpen: true}; // visible by default
 
-        component = render(
-            <AppDataContext.Provider value={{appData, appState}}>
-                <TagDrawer/>
-            </AppDataContext.Provider>
-        )
-    });
-    
-    test('shows permanent drawer on large screen', () => {
-        // I know how to do this one but only because it renders large by default
-    });
+        // jsdom does not have window.matchMedia;
+        // it must be mocked for testing screen-responsive components.
+        global.matchMedia = jest.fn().mockImplementation(query => {
+            return {
+                matches: cssMediaQuery.match(query, {
+                    width: window.innerWidth
+                }),
+                addListener: jest.fn(),
+                removeListener: jest.fn()
+            };
+        });
 
-    test('shows temporary drawer on small screen', () => {
-        // This one is proving to be difficult to get working
+        // Set the screen size large by default
         Object.defineProperty(window, 'innerWidth', {
             writable: true,
             configurable: true,
-            value: 150,
+            value: 1500
         });
-
         window.dispatchEvent(new Event('resize'));
-
-        expect(screen.getByTestId('temporary-drawer')).toBeInTheDocument();
     });
     
-    test('temporary drawer opens and closes', () => {
-        // gotta figure out the above test first
+    test('shows permanent drawer on large screen', () => {
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
+        expect(screen.getByTestId('permanent-drawer')).toBeInTheDocument();
+        expect(screen.queryByTestId('temporary-drawer'))
+            .not.toBeInTheDocument();
     });
 
-    test('correctly renders FilterCheckbox components', () => {
+    test('shows temporary drawer on small screen', () => {
+        // Check drawer behaves properly on small screens
+        Object.defineProperty(window, 'innerWidth', {
+            writable: true,
+            configurable: true,
+            value: 150
+        });
+        window.dispatchEvent(new Event('resize'));
+
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
+        expect(screen.getByTestId('temporary-drawer')).toBeInTheDocument();
+        expect(screen.queryByTestId('permanent-drawer'))
+            .not.toBeInTheDocument();
+    });
+
+    test('temporary drawer responds to appState', () => {
+        Object.defineProperty(window, 'innerWidth', {
+            writable: true,
+            configurable: true,
+            value: 150
+        });
+        window.dispatchEvent(new Event('resize'));
+
+        appState = {drawerOpen: false};
+        const {rerender} = render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+        expect(screen.getByTestId('temporary-drawer'))
+            .toHaveClass('MuiModal-hidden');
+
+        appState = {drawerOpen: true};
+        rerender(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+        expect(screen.getByTestId('temporary-drawer'))
+            .not.toHaveClass('MuiModal-hidden');
+    });
+
+    test('correctly renders FilterCheckbox components on large screen', () => {
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
         const checkboxes = screen.getAllByRole('checkbox');
         expect(checkboxes.length).toEqual(appData.tagData.length);
     });
 
-    test('correctly renders TagEditor components', () => {
+    test('correctly renders TagEditor components on large screen', () => {
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
+        const editButton = screen.getByRole('switch');
+        fireEvent(
+            editButton,
+            new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true
+            })
+        );
+
+        const editors = screen.getAllByRole('textbox');
+        expect(editors.length).toEqual(appData.tagData.length);
+    });
+
+    test('correctly renders FilterCheckbox components on small screen', () => {
+        // Check drawer behaves properly on small screens
+        Object.defineProperty(window, 'innerWidth', {
+            writable: true,
+            configurable: true,
+            value: 150
+        });
+        window.dispatchEvent(new Event('resize'));
+
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
+        const checkboxes = screen.getAllByRole('checkbox');
+        expect(checkboxes.length).toEqual(appData.tagData.length);
+    });
+
+    test('correctly renders TagEditor components on small screen', () => {
+        // Check drawer behaves properly on small screens
+        Object.defineProperty(window, 'innerWidth', {
+            writable: true,
+            configurable: true,
+            value: 150
+        });
+        window.dispatchEvent(new Event('resize'));
+        
+        render(
+            <AppDataContext.Provider value={{appData, appState}}>
+                <TagDrawer/>
+            </AppDataContext.Provider>
+        );
+
         const editButton = screen.getByRole('switch');
         fireEvent(
             editButton,
