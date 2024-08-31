a7fa64eda5cb652e0f89fb85c7e82d125e00ca10
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-08-31 18:33:44 -0500

    test(Tag):  Added test Added an intial test to the Tag component.  Incorporated change recommended in issue #137.

--- Frontend/src/ImageView/BottomToolbar/ExcessTagsChip.test.js
+++ Frontend/src/ImageView/BottomToolbar/ExcessTagsChip.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { alpha, createTheme } from '@mui/material';
 
 import ExcessTagsChip from './ExcessTagsChip';
 
 
 describe('ExcessTagsChip', () => {
     test('renders correct number', () => {
+        const mockTheme = createTheme({
+            palette: {
+                primary: {
+                main: alpha('#333333', 0.6),
+                },
+            }
+        });
         for (let i = 1; i <= 100; i++) {
-            render(<ExcessTagsChip labelText={i.toString()}/>);
+            render(
+                <ExcessTagsChip labelText={i.toString()} theme={mockTheme}/>
+            );
             expect(screen.getByText('+' + i.toString())).toBeInTheDocument();
         };
     });
