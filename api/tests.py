1f0c591ff105d10dd23ccd832d349402ffb3f99d
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-09-04 16:10:43 -0500

    docs: Add comment for unused imports Unused imports are commented out to avoid triggering the linter.  Added a comment communicating this for affected files.

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,10 @@
 """Tests for the package.  (Not yet in use.)
 """
 
+"""
+Import below is unused, but may be required later on.
+Commenting out to avoid unused import flags from the linter.
+"""
 # from django.test import TestCase
 
 # Create your tests here.
