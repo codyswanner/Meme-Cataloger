970d3a213a02aa3782f326c59e6f2094a5686f9d
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-09-18 21:14:31 -0500

    refac(Thumbnail): use styled components Switched to using @emotion/styled for better ease of using pseudo-selectors like &:hover{} This also eliminated the need for two modules, that have now been simplified into Thumbnail.js and a useFirstFrame hook!

--- Frontend/src/MainPage/ImageList/Thumbnail.js
+++ Frontend/src/MainPage/ImageList/Thumbnail.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
+import styled from '@emotion/styled';
 
-import ImageThumbnail from './ImageThumbnail';
-import VideoThumbnail from './VideoThumbnail';
+import useFirstFrame from './useFirstFrame';
 import AppDataContext from '../../SupportingModules/AppDataContext';
 
 
@@ -10,48 +10,43 @@ export default function Thumbnail(props) {
 
   const selected = appState.selectedItems.includes(props.id) ? true : false;
 
-  const containerStyles = {
-    aspectRatio: 1/1,  // ensure square thumbnails
-    width: "98%",
-    overflow: "hidden",
-  };
-
-  const selectedContainerStyles = {
-    ...containerStyles,
-    padding: "0.9rem 0.9rem",
-    backgroundColor: "rgba(75, 95, 255, 0.6)"
-  };
-
-  const thumbnailStyles = {
-    display: "block",
-    width: "100%",
-    height: "100%",
-    objectFit: "cover"
-  };
-
-  const selectedThumbnailStyles = {
-    ...thumbnailStyles,
-    opacity: "0.5"
-  };
-
   const videoFileTypesRegex = /.+\.(mp4|mov|avi|mkv|wmv|flv|webm)/i;
   const imageFileTypesRegex = /.+\.(jpg|jpeg|png|webp|gif|bmp|svg)/i;
 
+  const ThumbnailImg = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: ${selected ? '0.6' : ''};
+
+    &:hover{opacity:0.8;};
+  `;
+
+  const ThumbnailContainer = styled.div`
+    aspect-ratio: 1/1;
+    width: 98%;
+    overflow: hidden;
+    background-color: ${selected ? 'rgba(75, 95, 255, 0.6)' : ''};
+    padding: ${selected ? '0.9rem' : ''};
+  `;
+
   if (videoFileTypesRegex.test(props.src)) {
+    const firstFrame = useFirstFrame(props.src);
     return(
-      <VideoThumbnail
-        {...props}
-        containerStyles = {selected ? selectedContainerStyles:containerStyles}
-        thumbnailStyles = {selected ? selectedThumbnailStyles:thumbnailStyles}
-      />
+      <a href={"http://" + window.location.host + "/image/" + props.id}>
+        <ThumbnailContainer data-testid={'video-thumbnail'}>
+            {firstFrame&&<ThumbnailImg src={firstFrame}/>}
+        </ThumbnailContainer>
+      </a>
     );
   } else if (imageFileTypesRegex.test(props.src)) {
     return(
-      <ImageThumbnail
-        {...props}
-        containerStyles = {selected ? selectedContainerStyles:containerStyles}
-        thumbnailStyles = {selected ? selectedThumbnailStyles:thumbnailStyles}
-      />
+      <a href={"http://" + window.location.host + "/image/" + props.id}>
+        <ThumbnailContainer data-testid={'img-thumbnail'}>
+          <ThumbnailImg src={props.src}/>
+        </ThumbnailContainer>
+      </a>
     );
   } else {
     console.error(`Source ${props.src} does not match expected file types`);
--- Frontend/src/MainPage/ImageList/useFirstFrame.js
+++ Frontend/src/MainPage/ImageList/useFirstFrame.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
-export default function ImageThumbnail(props){
+export default function useFirstFrame(source) {
     const [firstFrame, setFirstFrame] = useState();
 
     useEffect(() => {
@@ -10,7 +10,7 @@ export default function ImageThumbnail(props){
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
 
-            video.src = props.src;
+            video.src = source;
             video.preload = 'metadata';
 
             await new Promise(resolve => video.addEventListener(
@@ -31,14 +31,7 @@ export default function ImageThumbnail(props){
         };
 
         generateFirstFrame();
-    }, [props.src]);
+    }, []);
 
-    return(
-      <a href={"http://" + window.location.host + "/image/" + props.id}>
-        <div style={props.containerStyles} data-testid={'video-thumbnail'}>
-            {firstFrame&&<img style={props.thumbnailStyles} src={firstFrame}/>}
-        </div>
-      </a>
-    );
+    return firstFrame;
 };
-
