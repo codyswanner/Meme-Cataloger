61e6b75a042f37e0d2144a8ac5d5e9d63237891d
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-11-05 22:08:31 -0600

    feat (gallery): New Dialog! Added a dialog that will someday become the AddTagsDialog for the gallery view (and then probably the ImageView as well).  For now it's just a box that says "Yup I'm here" but after so long of no activity on this, something is something!  (We'll see if it picks up speed again now that I've broken the silence)

---
+++ Frontend/src/MainPage/ApplyTagsPopper.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function ApplyTagsPopper(props) {
+  return(
+    <>
+    <div>
+      <dialog open={props.open}>
+        <p>Yup I'm here</p>
+      </dialog>
+    </div>
+    </>
+  );
+};
