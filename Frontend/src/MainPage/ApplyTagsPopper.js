8fb3622ce23bb549be8c7b8502e9c43d272471a1
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-11-12 14:16:44 -0600

    feat(ApplyTagsPopper): indeterminate checkboxes Now the checkboxes reflect overlaps and differences of tags applied to selected images (easier seen than said)

--- Frontend/src/MainPage/ApplyTagsPopper.js
+++ Frontend/src/MainPage/ApplyTagsPopper.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
 import AppDataContext from '../SupportingModules/AppDataContext';
 
@@ -8,8 +8,10 @@ export function TagCheckbox(props) {
   const { appData, appState } = props.appContext;
   const inputRef = useRef(null);
 
-  const handleSetup = (selection) => {
-    
+  useEffect(() => {
+
+    const checkbox = document.querySelector(`#checkbox-${props.tagId}`);
+    const selection = appState.selectedItems;
     const matchingTags = appData.imageTagData.filter(
       (imageTag) => {
         for (let i = 0; i < selection.length; i++){
@@ -21,12 +23,16 @@ export function TagCheckbox(props) {
     ).filter(
       (imageTag) => {return imageTag.tag_id == props.tagId}
     );
-    
-    if (matchingTags.length > 0) {return true} else {return false};
 
-    // Also need a partial match option?
-    // ^ This is a goal for another day
-  };
+    if (matchingTags.length == 0) {
+      checkbox.checked = false;
+    } else if (matchingTags.length == selection.length) {
+      checkbox.checked = true;
+    } else if (matchingTags.length > 0) {
+      checkbox.indeterminate = true;
+    };
+  
+  }, [appState.selectedItems]);
   
   const handleLabelClick = () => {
     if (inputRef.current.checked) {
@@ -40,8 +46,8 @@ export function TagCheckbox(props) {
     <>
       <input
         type='checkbox'
-        defaultChecked={handleSetup(appState.selectedItems)}
         name={props.tagId}
+        id={`checkbox-${props.tagId}`}
         value={props.tagName}
         ref={inputRef}
       />
