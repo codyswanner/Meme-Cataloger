ccecb876d2ecc0c1b35df34c3d010e1a1a86abed
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2024-11-27 16:45:33 -0600

    feat: Reset selection For the ApplyTagsPopper, the checkboxes now remember their initial state and allow the user to revert back to it for that individual checkbox.  Ideally this will also open up the possibility for a form-wide reset button.

--- Frontend/src/MainPage/TagCheckbox.js
+++ Frontend/src/MainPage/TagCheckbox.js
@@ -1,10 +1,14 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import styled from '@emotion/styled';
+
 import AppDataContext from '../SupportingModules/AppDataContext';
 
 
 export default function TagCheckbox(props) {
 
   const { appData, appState } = useContext(AppDataContext);
+  const [fieldChanged, setFieldChanged] = useState(false);
+  const initialValueRef = useRef(null);
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -23,13 +27,23 @@ export default function TagCheckbox(props) {
       (imageTag) => {return imageTag.tag_id == props.tagId}
     );
 
+    // Set initial value, and save in a ref (for undo/reset)
     if (matchingTags.length == 0) {
+      // If selected images do not have this tag, leave unchecked by default.
       checkbox.checked = false;
+      initialValueRef.current = false;
     } else if (matchingTags.length == selection.length) {
+      // If all selected images have this tag, show checked by default.
       checkbox.checked = true;
+      initialValueRef.current = true;
     } else if (matchingTags.length > 0) {
+      // If only some images have this tag, show indeterminate checkbox.
       checkbox.indeterminate = true;
+      initialValueRef.current = 'indeterminate';
     };
+    
+    setFieldChanged(false);
+    console.log("Fields reset!");
   
   }, [appState.selectedItems]);
   
@@ -39,16 +53,58 @@ export default function TagCheckbox(props) {
     } else {
       inputRef.current.checked = true;
     };
+    handleChange(); // Yo JS, kinda stupid that I gotta do this
   };
+
+  const handleChange = () => {
+    if (!fieldChanged) {
+      setFieldChanged(true);
+      console.log("Field changed!");
+    } else {
+      console.log("Field already changed");
+    };
+  };
+
+  const handleResetClick = (e) => {
+    // Do not submit the form
+    e.preventDefault();
+
+    //  Reset the state
+    setFieldChanged(false);
+    
+    // Reset the checkbox to it's original value
+    const checkbox = document.querySelector(`#checkbox-${props.tagId}`);
+    if (initialValueRef.current == false) {
+      checkbox.checked = false;
+    } else if (initialValueRef.current == true) {
+      checkbox.checked = true;
+    } else if (initialValueRef.current == 'indeterminate') {
+      checkbox.indeterminate = true;
+    };
+
+  };
+
+  const ResetButton = styled.button`
+    padding: 0;
+    background: rgba(0, 0, 0, 0);
+    border: 0;
+  `;
 
   return(
     <>
+      {
+        fieldChanged &&
+        <ResetButton onClick={(e) => handleResetClick(e)}>
+          <img src="../../static/frontend/undo.png" height='12em'/>
+        </ResetButton>
+      }
       <input
         type='checkbox'
         name={props.tagId}
         id={`checkbox-${props.tagId}`}
         value={props.tagName}
         ref={inputRef}
+        onChange={() => handleChange()}
       />
       <label htmlFor={props.tagId} onClick={handleLabelClick}>
         {props.tagName}
