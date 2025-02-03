0bd6864173df9a1359c134c2ec6a4501284c361b
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2025-02-03 09:27:17 -0600

    fix(lint): remove unused import

--- Frontend/src/MainPage/TagDrawer/ExactMatchSwitch.js
+++ Frontend/src/MainPage/TagDrawer/ExactMatchSwitch.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 
 import AppDataContext from '../../SupportingModules/AppDataContext';
 import filterSocket from '../../SupportingModules/FilterSocket';
